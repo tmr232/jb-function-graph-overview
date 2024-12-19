22426b31e5659de511a87f48d8c7309fe74798a1
Tamir Bahar <tamir.bahar@gmail.com>
2024-12-19 19:08:39 +0200

    Remove incorrect copyright notice

--- src/main/kotlin/com/github/tmr232/function_graph_overview/settings/Settings.kt
+++ src/main/kotlin/com/github/tmr232/function_graph_overview/settings/Settings.kt
@@ -1,4 +1,3 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.github.tmr232.function_graph_overview.settings
 
 import com.intellij.openapi.application.ApplicationManager
