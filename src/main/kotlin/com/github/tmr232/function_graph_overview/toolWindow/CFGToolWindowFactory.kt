5101dbe4029cbebafe2fa2edd5776c1afe3df550
Tamir Bahar <tamir.bahar@gmail.com>
2025-02-17 15:00:05 +0200

    Remove debug button

--- src/main/kotlin/com/github/tmr232/function_graph_overview/toolWindow/CFGToolWindowFactory.kt
+++ src/main/kotlin/com/github/tmr232/function_graph_overview/toolWindow/CFGToolWindowFactory.kt
@@ -20,7 +20,6 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
-import com.intellij.ui.components.JBPanel
 import com.intellij.ui.jcef.JBCefApp
 import com.intellij.ui.jcef.JBCefJSQuery.Response
 import javax.swing.JButton
@@ -191,11 +190,11 @@ class CFGToolWindowFactory :
 
     private fun createWebViewContent(): JComponent {
 //         TODO: Find a way to enable debugging and keep the scaling!
-//        return localBrowser.component
-        return JBPanel<JBPanel<*>>().apply {
-            add(createButton("Open Devtools") { localBrowser.openDevtools() })
-            add(localBrowser.component)
-        }
+        return localBrowser.component
+//        return JBPanel<JBPanel<*>>().apply {
+//            add(createButton("Open Devtools") { localBrowser.openDevtools() })
+//            add(localBrowser.component)
+//        }
     }
 }
 
